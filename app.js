38be22b2740bbf1e6569e2545008ca94c7f8d6bd
Hari Irawan <hari31121993@gmail.com>
2020-01-28 18:10:11 +0700

    update file

--- app.js
+++ app.js
@@ -1,79 +1,85 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import multer from 'multer';
-import csv from 'csv-parser';
-import stream from 'stream';
-import QRCode from 'qrcode';
-import fs from 'fs';
-import { generateCanvas, readTotalQR } from './helpers';
+import express from "express";
+import bodyParser from "body-parser";
+import multer from "multer";
+import csv from "csv-parser";
+import stream from "stream";
+import QRCode from "qrcode";
+import fs from "fs";
+import { generateCanvas, readTotalQR, deleteFolderRecursive } from "./helpers";
 
-const path = require('path');
+const path = require("path");
 
-const zl = require('zip-lib');
+const zl = require("zip-lib");
 const app = express();
 const storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  if (!fs.existsSync(`./assets/qrcode/air`)) {
+    fs.mkdirSync(`./assets/qrcode/air`);
+  }
+
+  if (!fs.existsSync(`./assets/qrcode/listrik`)) {
+    fs.mkdirSync(`./assets/qrcode/listrik`);
+  }
+
+  res.render("index");
 });
 
-app.get('/testing', function(req, res, next) {
-  var filePath = './assets'; // Or format the path using the `id` rest param
-  var fileName = 'qrcode.zip'; // The default name the browser will use
+app.get("/testing", function(req, res, next) {
+  var filePath = "./assets"; // Or format the path using the `id` rest param
+  var fileName = "qrcode.zip"; // The default name the browser will use
   res.download(path.join(filePath, fileName));
 });
 
-app.post('/qrcode', upload.single('csvFile'), async (req, res, next) => {
+app.post("/qrcode", upload.single("csvFile"), async (req, res, next) => {
   const { folder } = req.body;
   const { buffer: bufferCSV } = req.file;
-
-  if (!fs.existsSync(`./assets/qrcode/${folder}`)) {
-    fs.mkdirSync(`./assets/qrcode/${folder}`);
-  }
-
   const bufferStream = new stream.PassThrough();
   bufferStream.end(new Buffer.from(bufferCSV));
   await bufferStream
     .pipe(csv())
-    .on('data', function(data) {
+    .on("data", function(data) {
       try {
-        //format qr code unitID;unitName;floorName;towerName;billTypeID;billTypeName
         let dataQrCode = `${data.unitID};${data.unitName};${data.floorName};${data.towerName};${data.biilTypeID};${data.billTypeName}`;
 
         const options = {
-          errorCorrectionLevel: 'L',
+          errorCorrectionLevel: "L",
           scale: 5,
           margin: 2,
           maskPattern: 2,
-          width: 76,
+          width: 76
         };
         QRCode.toDataURL(dataQrCode, options, function(_, url) {
-          generateCanvas(url, `${data.unitName.split('/').join('-')}-${data.billTypeName}`, folder);
+          generateCanvas(
+            url,
+            `${data.unitName.split("/").join("-")}-${data.billTypeName}`,
+            folder
+          );
         });
       } catch (err) {
         console.log(err);
       }
     })
-    .on('end', function() {
-      console.log('Generate qr code selesai');
+    .on("end", function() {
+      console.log("Generate qr code selesai");
     });
 
-  await zl.archiveFolder('./assets/qrcode', './assets/qrcode.zip').then(
+  await zl.archiveFolder("./assets/qrcode", "./assets/qrcode.zip").then(
     function() {
-      console.log('done');
+      console.log("done");
     },
     function(err) {
       console.log(err);
-    },
+    }
   );
 
   await readTotalQR();
 });
 
-app.listen(1994, () => console.log('Server started at port 1994'));
+app.listen(1994, () => console.log("Server started at port 1994"));
--- helpers.js
+++ helpers.js
@@ -1,32 +1,34 @@
-import { createCanvas, Image } from 'canvas';
-import fs from 'fs';
+import { createCanvas, Image } from "canvas";
+import fs from "fs";
 
 export function generateCanvas(url, name, folder) {
   const canvas = createCanvas(76, 106);
   const stream = canvas.createPNGStream();
-  const ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext("2d");
 
   ctx.rect(0, 0, 76, 106);
-  ctx.fillStyle = '#000000';
+  ctx.fillStyle = "#000000";
   ctx.fill();
 
-  ctx.strokeStyle = 'black';
+  ctx.strokeStyle = "black";
   ctx.strokeRect(0, 0, 76, 76);
   const img = new Image();
-  const out = fs.createWriteStream(`./assets/qrcode/${folder}/` + `${name}.png`);
+  const out = fs.createWriteStream(
+    `./assets/qrcode/${folder}/` + `${name}.png`
+  );
   img.onload = () => ctx.drawImage(img, 0, 0);
   img.onerror = err => {
     throw err;
   };
   img.src = url;
 
-  ctx.font = '8px arial';
-  ctx.textAlign = 'center';
-  ctx.fillStyle = 'white';
+  ctx.font = "8px arial";
+  ctx.textAlign = "center";
+  ctx.fillStyle = "white";
   ctx.fillText(name, 76 / 2, 95, 106);
 
   stream.pipe(out);
-  out.on('finish', () => {});
+  out.on("finish", () => {});
 }
 
 export function readTotalQR() {
@@ -34,3 +36,20 @@ export function readTotalQR() {
   var listrik = fs.readdirSync(`./assets/qrcode/listrik`);
   console.log(`Success generate qr code: `, air.length, listrik.length);
 }
+
+export const deleteFolderRecursive = function(path) {
+  console.log("Jalsn");
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach((file, index) => {
+      const curPath = Path.join(path, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        // recurse
+        deleteFolderRecursive(curPath);
+      } else {
+        // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
